ee07f7844e2758d602e8cc835471ed61c5fbf2d0
coderjibon <contact.engjibon@gmail.com>
2021-09-14 16:52:28 +0600

    services page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,17 @@
         prevArrow:false,
         nextArrow:false
       });
+
+      $('.testimonialsSlider2').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        nextArrow: '<button type="button" class="slick-next"><img src="assets/images/next.png" alt=""></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="assets/images/prev.png" alt=""></button>'
+      });
+
      
 })(jQuery);
+
+
+
